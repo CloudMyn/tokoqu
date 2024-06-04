6020186cc6d2c1371cf429faa2a7f04887e79318
cloudmyn <cloudmyn46@gmail.com>
2024-06-04 13:44:24 +0800

    🧹 chore: init tokoqu

---
+++ database/migrations/2024_06_04_050944_create_employees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_code')->unique();
+            $table->string('full_name');
+            $table->string('ktp_number', 16);
+            $table->string('ktp_photo');
+            $table->timestamp('start_working_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
